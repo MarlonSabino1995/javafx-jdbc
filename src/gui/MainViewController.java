65359f7c7de1e70272326b508a9d5e1481e86232
Marlon Sabino <sabinomarlon1@gmail.com>
2025-08-09 12:46:30 -0300

    Tela Sobre

---
+++ src/gui/MainViewController.java
@@ -0,0 +1,69 @@
+package gui;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import application.Main;
+import gui.util.Alerts;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.VBox;
+
+public class MainViewController implements Initializable {
+
+	@Override
+	public void initialize(URL url, ResourceBundle rb) {
+		
+	}
+	
+	@FXML
+	private MenuItem menuItemVendedor;
+	
+	@FXML
+	private MenuItem menuItemDepartamento;
+	
+	@FXML
+	private MenuItem menuItemAbout;
+
+	@FXML
+	public void onMenuItemVendedorAction() {
+		
+	}
+	@FXML
+	public void onMenuItemDepartamentoAction() {
+		
+	}
+	@FXML
+	public void onMenuItemAboutAction() {
+		loadView("/gui/Sobre.fxml");
+	}
+	
+	private synchronized void loadView(String nomeAbsoluto) {
+		try {
+			FXMLLoader loader = new FXMLLoader(getClass().getResource(nomeAbsoluto));
+			VBox newVbox = loader.load();
+			
+			Scene mainScene = Main.getMainScene();
+			VBox mainVBox = (VBox)((ScrollPane)mainScene.getRoot()).getContent();
+			
+			
+			 
+			Node mainMenu = mainVBox.getChildren().get(0);
+			mainVBox.getChildren().clear();
+			mainVBox.getChildren().add(mainMenu);
+			mainVBox.getChildren().addAll(newVbox.getChildren());
+			
+			
+		}catch(IOException e) {
+			Alerts.showAlert("IO Exception", "Erro ao carregar a página", e.getMessage(), AlertType.ERROR);
+		}
+	}
+	
+}
---
+++ src/model/Vendedor.java
@@ -0,0 +1,16 @@
+package model;
+
+import java.io.Serializable;
+
+public class Vendedor implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+
+	
+	private Integer matricula;
+	private String nome;
+	private String email;
+	private Departamento departamento;
+	
+	
+}
